173cf061de4cdbc4bd281a3e3d3081cbcca950ab
LeTHung <letienhung2108@gmail.com>
2025-09-24 10:19:53 +0700

    bo sung bai tap chuong4

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import StudentCard from "./StudentCard";
+import { useState } from "react";
 
 function App() {
   const student = { name: "Lê Văn C", age: 19, className: "CNTT3" };
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>Danh sách sinh viên</h1>
+      <div>
+        <p>Giá trị hiện tại: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Next</button>
+      </div>
 
+      <h1>Danh sách sinh viên</h1>
       {/* Cách 1: Truyền props trực tiếp */}
       <StudentCard name="Nguyễn Văn A" age={20} className="CNTT1" />
-
       {/* Cách 2: Truyền biến */}
       <StudentCard
         name={student.name}
         age={student.age}
         className={student.className}
       />
-
       {/* Cách 3: Spread props (gọn hơn) */}
       <StudentCard {...student} />
     </div>
